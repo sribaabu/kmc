0f393715a853e67d423a78b42e3f1a1deed2c451
sribaabu <sribaabu@gmail.com>
2021-06-28 19:41:23 +0530

    New Resources Added

---
+++ Confluent Schema Registry/SampleSchemaRegistryPrograms/SchemaRegistryVersion1/src/main/java/com/sribaabu/schemaregistry/version1/SchemaRegistryConsumerVersion1.java
@@ -0,0 +1,60 @@
+package com.sribaabu.schemaregistry.version1;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import com.sribaabu.Employee;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+
+/**
+ * Hello world!
+ *
+ */
+public class SchemaRegistryConsumerVersion1 {
+	public static void main(String[] args) {
+
+		// Configuring Normal Consumer
+
+		Properties properties = new Properties();
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+//      DeSerializer Should be used as Kafka Avro DeSerializer
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+		properties.setProperty("specific.avro.reader", "true");
+
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "sribaabu-app");
+		properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+//        The Actual Schema Registry Server URL
+		properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
+
+		KafkaConsumer<String, Employee> kafkaConsumer = new KafkaConsumer<>(properties);
+
+		String topic = "sribaabu-schema-registry-topic";
+		kafkaConsumer.subscribe(Collections.singleton(topic));
+
+		System.out.println("Waiting for the data from kafka ...");
+
+		while (true) {
+			System.out.println("Polling Kafka");
+			ConsumerRecords<String, Employee> records = kafkaConsumer.poll(1000);
+
+			for (ConsumerRecord<String, Employee> record : records) {
+				Employee employee = record.value();
+				System.out.println(employee);
+			}
+
+			kafkaConsumer.commitSync();
+		}
+	}
+
+}
---
+++ Confluent Schema Registry/SampleSchemaRegistryPrograms/SchemaRegistryVersion1/src/main/java/com/sribaabu/schemaregistry/version1/SchemaRegistryProducerVersion1.java
@@ -0,0 +1,79 @@
+package com.sribaabu.schemaregistry.version1;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import com.sribaabu.Employee;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+
+/**
+ * Hello world!
+ *
+ */
+public class SchemaRegistryProducerVersion1 
+{
+	public static void main(String[] args) {
+		
+        // Configuring Normal Producer
+		
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        
+//        Serializer Should be used as Kafka Avro Serializer
+        
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        
+//        The Actual Schema Registry Server URL
+        properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
+
+        
+        KafkaProducer<String, Employee> producer = new KafkaProducer<String, Employee>(properties);
+
+        String topic = "sribaabu-schema-registry-topic";
+
+//        Constructing Employee Object with the Generation Avro Model Class 
+        
+        Employee employee = Employee.newBuilder()
+                .setAge(34)
+                .setAutomatedEmail(false)
+                .setFirstName("Sribaabu")
+                .setLastName("Murthy")
+                .setHeight(178f)
+                .setWeight(75f)
+                .setCity("Erode")
+                .setCountry("India")
+                .build();
+
+        ProducerRecord<String, Employee> producerRecord = new ProducerRecord<String, Employee>(
+                topic, employee
+        );
+
+        System.out.println(employee);
+        
+//        Sending the Serialized Data to the Topic
+        
+        producer.send(producerRecord, new Callback(){
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if (exception == null) {
+                    System.out.println(metadata);
+                } else {
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        producer.flush();
+        producer.close();
+
+    }
+}
